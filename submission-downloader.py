f2f961f137d9201b9bf2dba0981f25f0ea7887e2
cg <31290848+ComputerGenius152@users.noreply.github.com>
2020-06-18 23:03:21 -0400

    bugfix

--- submission-downloader.py
+++ submission-downloader.py
@@ -39,14 +39,15 @@ class SubmissionDownloader:
 
     def get_submission_sources(self, submissions): # downloads submissions, filters out the best
         if self.overwrite == True:
-            shutil.rmtree("downloaded-submissions")
-            os.mkdir("downloaded-submissions")
-            os.chdir("downloaded-submissions")
+            try:
+                shutil.rmtree("downloaded-submissions")
+                os.mkdir("downloaded-submissions")
+            finally:
+                os.chdir("downloaded-submissions")
         else:
             try:
                 os.mkdir("downloaded-submissions")
-                os.chdir("downloaded-submissions")
-            except:
+            finally:
                 os.chdir("downloaded-submissions")
         if self.aconly == True:
             submissions = [thing for thing in submissions if thing[4] == "AC" or thing[4] == "_AC"]
